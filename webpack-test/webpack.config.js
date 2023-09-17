9e7e553220929158dca92dd2c8762877b0330211
KimCookieYa <min49590@gmail.com>
2023-09-17 19:06:49 +0900

    Add: webpack babel-preset 추가

--- webpack-test/webpack.config.js
+++ webpack-test/webpack.config.js
@@ -16,7 +16,19 @@ module.exports = {
         test: /\.jsx?$/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["last 2 chrome versions"],
+                  debug: true,
+                },
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugin: [],
         },
       },
     ],
